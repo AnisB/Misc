1c308a50893876c0e2f0d3313f4f3e64e7535efb
Anis Benyoub <anisbenyoub@MacBook-Pro-de-Anis.local>
2014-12-12 14:55:32 -0500

    ds

--- doublestar.cpp
+++ doublestar.cpp
@@ -1,34 +1,21 @@
-// Ok  donc l'idée c'est ça: 
 #include <iostream>
 
-// void dude( void* coucou)
-// {
-//         float** buf = (float**)coucou;
-//         buf;
-//         buf[0];
-//        // buf[0][0];
+void dude( void* coucou, int sizeX, int sizeY)
+{
+        float* buf = (float*)coucou;
+        for(int i = 0; i < sizeY; ++i)
+        {
+	        for(int j = 0; j < sizeX; ++j)
+	        {
+	        	std::cout<<buf[i*sizeY+j]<<" ";
+	        }
+	        std::cout<<std::endl;
+        }
 
-// }
+}
 int main()
 {
         float buf [2][2] = {{1,2},{3,4}};
-        // buf;
-        // buf[0];
-        // dude((void*)buf);
+        dude((void*)buf, 2, 2);
         return 0;
 }
-
-
-// #include <iostream>
-
-// void hi(float* coucoutheo)
-// {
-//     coucoutheo[0];
-// }
-
-// int main()
-// {
-//     float hihi[2] = {1,2};
-//     hi(hihi);
-//     return 0;
-// }
